dcd7fe282b7e3c66f45d7ac098cbc664588a5d68
sam <sam@spoonradio.co>
2021-03-26 15:13:52 +0900

    Add pyenv

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
 brew install tmux
 brew install vim
 brew install fzf
 brew install ack
+brew install pyenv
 
 # git auto-complete
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash

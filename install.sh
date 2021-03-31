6434a9bcb6d66d694e4d17ebbb75d8e72dd55d26
sam <sam@spoonradio.co>
2021-03-31 14:08:14 +0900

    Add ripgrep

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ brew install fzf
 /usr/local/opt/fzf/install
 brew install ack
 brew install pyenv
+brew install ripgrep
 
 # git auto-complete
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
